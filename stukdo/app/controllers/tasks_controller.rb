1279af5cd6d178a0e359e704840dabdbdb9edf06
John B. Wyatt IV <johnwyatt@jbwyatt4.com>
2015-08-20 04:35:37 -0700

    Added change task custom action

--- stukdo/app/controllers/tasks_controller.rb
+++ stukdo/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
   before_action :authenticate_user!
 
   # GET /tasks
@@ -37,6 +37,13 @@ class TasksController < ApplicationController
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "Task Updated"}
     end
   end
 
